283c783c00ee4d7f43926129c9ad766016adc128
snowmerak <snowmerak@outlook.com>
2025-01-13 03:36:30 +0900

    implement CheckToken and Receive

--- pkg/receiver/receiver.go
+++ pkg/receiver/receiver.go
@@ -1,11 +1,51 @@
 package receiver
 
 import (
+	"context"
+	"fmt"
+	"time"
+
 	"github.com/snowmerak/DM/lib/auth"
+	"github.com/snowmerak/DM/lib/auth/encrypt"
 	"github.com/snowmerak/DM/lib/broker"
 )
 
-type Recevier struct {
+type Receiver struct {
+	keyStore auth.KeyStore
 	verifier auth.Verifier
 	broker   broker.Broker
+}
+
+func New(verifier auth.Verifier, broker broker.Broker) *Receiver {
+	return &Receiver{
+		verifier: verifier,
+		broker:   broker,
+	}
+}
+
+func (r *Receiver) CheckToken(ctx context.Context, keyId string, message []byte, signature []byte) error {
+	algorithm, parsedPublicKey, err := r.keyStore.Get(ctx, keyId, auth.KeyTypePublic)
+	if err != nil {
+		return fmt.Errorf("failed to get public key: %w", err)
+	}
+
+	publicKey, err := encrypt.UnmarshalPublicKey(algorithm, parsedPublicKey)
+	if err != nil {
+		return fmt.Errorf("failed to unmarshal public key: %w", err)
+	}
+
+	if err := r.verifier.Verify(publicKey, signature, message); err != nil {
+		return fmt.Errorf("failed to verify signature: %w", err)
+	}
+
+	return nil
+}
+
+func (r *Receiver) Receive(ctx context.Context, namespace string, groupId string, sender string, message []byte, publishedAt time.Time) error {
+	if err := r.broker.Publish(ctx, namespace, groupId, sender, message, publishedAt); err != nil {
+		return fmt.Errorf("failed to publish message: %w", err)
+	}
+
+	return nil
+
 }
