951ad4491375dcf2959a125f33a80a0c57606158
snowmerak <snowmerak@outlook.com>
2025-01-13 03:35:06 +0900

    add AllGroups constant

--- lib/broker/broker.go
+++ lib/broker/broker.go
@@ -1,8 +1,15 @@
 package broker
 
-import "context"
+import (
+	"context"
+	"time"
+)
+
+const (
+	AllGroups = "*"
+)
 
 type Broker interface {
-	Publish(ctx context.Context, namespace string, groupId string, message []byte) error
-	Subscribe(ctx context.Context, namespace string, groupId string, callback func(ctx context.Context, namespace string, groupId string, message []byte) error) error
+	Publish(ctx context.Context, namespace string, groupId string, sender string, message []byte, publishedAt time.Time) error
+	Subscribe(ctx context.Context, namespace string, groupId string, callback func(ctx context.Context, namespace string, groupId string, sender string, message []byte, publishedAt time.Time) error) error
 }
