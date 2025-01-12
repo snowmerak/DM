9fe7fc9c2d820aece3c02eb99adf8694b8ddbbbf
snowmerak <snowmerak@outlook.com>
2025-01-13 03:35:39 +0900

    add namespace to interface methods

--- lib/message/message.go
+++ lib/message/message.go
@@ -13,16 +13,16 @@ type Message struct {
 }
 
 type Storage interface {
-	Save(ctx context.Context, groupId string, sender string, message []byte, publishedAt time.Time) error
-	Search(ctx context.Context, groupId string, startPublishedAt, endPublishedAt time.Time) ([]*Message, error)
-	SearchViaSender(ctx context.Context, groupdId string, sender string, startPublishedAt, endPublishedAt time.Time) ([]*Message, error)
+	Save(ctx context.Context, namespace string, groupId string, sender string, message []byte, publishedAt time.Time) error
+	Search(ctx context.Context, namespace string, groupId string, startPublishedAt, endPublishedAt time.Time) ([]*Message, error)
+	SearchViaSender(ctx context.Context, namespace string, groupId string, sender string, startPublishedAt, endPublishedAt time.Time) ([]*Message, error)
 }
 
 type Indexer interface {
-	Create(ctx context.Context, groupId string) error
-	Insert(ctx context.Context, groupdId string, sender string, message []byte, publishedAt time.Time) error
-	DeleteOne(ctx context.Context, groupdId string, sender string, pblishedAt time.Time) error
-	Drop(ctx context.Context, groupdId string) error
-	DeleteRange(ctx context.Context, groupdId string, startTimestamp time.Time, endTimestamp time.Time) error
-	Search(ctx context.Context, groupId string, sender string, query string) ([]*Message, error)
+	Create(ctx context.Context, namespace string, groupId string, temporaryNumber int64) error
+	Insert(ctx context.Context, namespace string, groupId string, temporaryNumber int64, sender string, message []byte, publishedAt time.Time) error
+	DeleteOne(ctx context.Context, namespace string, groupId string, temporaryNumber int64, sender string, publishedAt time.Time) error
+	Drop(ctx context.Context, namespace string, groupId string, temporaryNumber int64) error
+	DeleteRange(ctx context.Context, namespace string, groupId string, temporaryNumber int64, startTimestamp time.Time, endTimestamp time.Time) error
+	Search(ctx context.Context, namespace string, groupId string, temporaryNumber int64, query string) ([]*Message, error)
 }
